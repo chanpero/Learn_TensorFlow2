a444103a4cca61066dc540b5db7dd780829f144a
chanpero <qianchaosolo@gmail.com>
2021-03-26 23:13:42 +0800

    add class03 codes

---
+++ Class03/Fashion_Class.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Flatten
+
+
+class FashionModel(Model):
+    def __init__(self):
+        super(FashionModel, self).__init__()
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10, activation='softmax')
+
+    def call(self, x, training=None, mask=None):
+        x = self.flatten(x)
+        x = self.d1(x)
+        y = self.d2(x)
+        return y
+
+
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
+x_train, y_train = x_train / 255.0, y_train / 255.0
+
+model = FashionModel()
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
+model.summary()
---
+++ Class03/Fashion_Sequential.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
+
+# plt.imshow(x_train[67])
+# plt.show()
+# print('The pic is ', y_train[67])
+
+# print('x_train[1]: \n', x_train[0])
+# print('y_train[1]: \n', y_train[0])
+#
+# print("x_train.shape:\n", x_train.shape)
+# print("y_train.shape:\n", y_train.shape)
+# print("x_test.shape:\n", x_test.shape)
+# print("y_test.shape:\n", y_test.shape)
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
+
+model.summary()
